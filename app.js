2d8f8c99b9d2147b09cecbfa0e29128def3362d5
Siya <ayispauljc@gmail.com>
2024-03-02 22:02:49 +0530

    Updated

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const viewRouter = require('./routers/viewRouter')
 const app = express()
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+console.log(DB);
+async function connection() {
+  try {
+    await mongoose.connect(DB);
+    console.log('Connected to DB');
+  } catch (error) {
+    console.error(error);
+  }
+}
+connection();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));

3bd0271dc681361544dcb49eb3c68196c484ee79
Siya <ayispauljc@gmail.com>
2024-03-02 15:43:28 +0530

    InitialCommit

---
+++ public/script.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     var images = ["url('./images/image1.png')", "url('./images/image2.png')", "url('./images/image3.png')"];
+//     var currentIndex = 0;
+//     var imageContainer = document.querySelector("body");
+
+//     function changeImage() {
+//         currentIndex = (currentIndex + 1) % images.length;
+//         console.log(images[currentIndex])
+//         imageContainer.style.backgroundImage = images[currentIndex];
+//     }
+
+//     changeImage(); // Change image initially
+//     setInterval(changeImage, 10000); // Change image every 30 seconds (30000 milliseconds)
+// });

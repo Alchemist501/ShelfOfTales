0df923f91f08cdbba351e75d3ebfb1050e1a422d
Siya <ayispauljc@gmail.com>
2024-03-02 17:02:57 +0530

    Updated

---
+++ controllers/viewControllers.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const catchAsync = require('./../utils/catchAsync')
+exports.getOverview = catchAsync(async (req, res, next) => {
+    res.status(200).render('overview', {
+      title: 'ShelfOfTales'
+    });
+    next();
+  });
---
+++ routers/viewRouter.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const Controller = require('./../controllers/viewControllers')
+const router = express.Router()
+router.get('/',  Controller.getOverview);
